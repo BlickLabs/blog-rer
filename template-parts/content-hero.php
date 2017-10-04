028dddab8c0ad150a901d55745bd003a8a2d08ba
Raúl Marty <santor3medio@gmail.com>
2017-10-04 16:03:14 -0500

    Change position of text date and add class for modify of title post in content-hero.php

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -13,11 +13,17 @@ if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 ?>
 
  <div class="entry-hero" <?php shoreditch_background_image(); ?>>
-   <div class="entry-hero-wrapper">
+   
+ </div><!-- .entry-hero -->
+
+ <div class="entry-hero-wrapper">
      <?php
      shoreditch_entry_meta();
  
-     the_title( '<h1 class="entry-title">', '</h1>' );
+     the_title( '<h1 class="entry-title entry-title-hero rer-font-roboto-regular">', '</h1>' );
      ?>
    </div><!-- .entry-hero-wrapper -->
- </div><!-- .entry-hero -->
+
+<footer class="entry-footer entry-footer-hero">
+			<?php shoreditch_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
